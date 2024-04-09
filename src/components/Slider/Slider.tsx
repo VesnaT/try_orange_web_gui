a48adeeeca7f94bde2a7763a0a340a4e98fb9da0
Vesna Tanko <tankovesna@hotmail.com>
2024-04-09 10:21:30 +0200

    Slider: Form grouping

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -23,9 +23,11 @@ const Slider = ({
   return (
     <Tooltip tooltip={tooltip}>
       <Form>
-        <Form.Label>{text}</Form.Label>
-        <Form.Range onChange={_onChange} disabled={disabled}/>
-        <Form.Label>{value}</Form.Label>
+        <Form.Group className="mb-3" controlId="Slider">
+          <Form.Label>{text}</Form.Label>
+          <Form.Range onChange={_onChange} disabled={disabled} />
+          <Form.Label>{value}</Form.Label>
+        </Form.Group>
       </Form>
     </Tooltip>
   );
